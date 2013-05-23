12b4a40a0c833ef9b63ab33e6fe0ba22c41fcffb
Owyn Richen <orichen@owyricm0.corp.w3data.com>
2013-05-23 15:54:04 -0700

    updating the recipe descriptions in the metadata.rb file

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,8 @@ license          "Apache 2.0"
 description      "Installs/Configures pgbouncer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
-recipe           "pgbouncer", "Example usage of pgbouncer resource"
+recipe           "pgbouncer", "the default recipe does nothing to hopefully indicate that this is an LWRP cookbook"
+recipe		 "pgbouncer::example", "this gives an example of how one could consume the pgbouncer cookbook"
 
 %w{ubuntu}.each do |os|
   supports os
